199a4923685aac52c54ff4a32dd6c696caa0ab91
Alex <gr4k@bk.ru>
2017-10-15 00:25:33 +0300

    fix

--- alar_task_2/settings.py
+++ alar_task_2/settings.py
@@ -119,5 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_RESULT_BACKEND = 'django-cache'
